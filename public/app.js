b71846887e25b3c416084ececfc37a86711f0ce7
minoga2000 <minoga2000@gmail.com>
2016-02-24 00:04:06 +0300

    Add cart_get_orders

--- public/app.js
+++ public/app.js
@@ -24,6 +24,25 @@ function something()
 	function cart_get_number_of_items()
 	{
 		var cnt = 0;
+	
+		for(var i = 0; i < window.localStorage.length; i++)
+		{
+			var key = window.localStorage.key(i); // получаем ключ
+			var value = window.localStorage.getItem(key); // получаем значениеб в ruby: hh[key] = x
+				
+			if(key.indexOf('product_') == 0)
+			{
+				cnt = cnt + value * 1;
+			}
+		}
+	
+		return cnt;
+	}
+
+
+	function cart_get_orders()
+	{
+		var orders = '';
 
 		for(var i = 0; i < window.localStorage.length; i++)
 		{
@@ -32,10 +51,10 @@ function something()
 			
 			if(key.indexOf('product_') == 0)
 			{
-				cnt = cnt + value * 1;
+				orders = orders + key + "=" + value + ',';
 			}
 		}
 
-		return cnt;
+		return orders;
 	}
 
