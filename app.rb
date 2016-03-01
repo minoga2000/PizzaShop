f6d5ae7fc08638f8f51d28f81a7bacaa82d74147
minoga2000 <minoga2000@gmail.com>
2016-03-01 16:03:42 +0300

    36 53:45

--- app.rb
+++ app.rb
@@ -24,6 +24,11 @@ get '/about' do
 	erb :about
 end
 
+post '/place_order' do
+	@order = Order.create params[:order]
+	erb "Thank you! Your order has been placed."
+end
+
 post '/cart' do
 	orders_input = params[:orders]
 	@items = parse_orders_input orders_input
